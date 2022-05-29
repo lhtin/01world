9d4e0cfb1c6a30c60384e27a1281ff989d79b63d
Lehua Ding <lehuading@qq.com>
2022-05-29 16:38:24 +0800

    update app/riscv

--- app/riscv/app.jsx
+++ app/riscv/app.jsx
@@ -1,16 +1,28 @@
 const RISCV_EXTENSIONS = new Set([
   "I", "M", "A", "F", "D", "Q", "C", "V",
-  "Zicsr", "Zifencei", "Zihintpause"
+  "Zicsr", "Zifencei"
+]);
+
+const G_EXTENSIONS = new Set([
+  "I", "M", "A", "F", "D", "Zicsr", "Zifencei"
 ]);
 
 const isAll = (extSet) => extSet.size == RISCV_EXTENSIONS.size;
 
+const hasG = (extSet) => Array.from(G_EXTENSIONS).every((ext) => extSet.has(ext));
+
 const toggle = (extSet, ext) => {
-  if (ext == "all") {
+  if (ext === "all") {
     if (isAll(extSet)) {
       return new Set();
     } else {
       return RISCV_EXTENSIONS;
+    }
+  } else if (ext === "g") {
+    if (Array.from(G_EXTENSIONS).every((ext) => extSet.has(ext))) {
+      return new Set(Array.from(extSet).filter((ext) => !G_EXTENSIONS.has(ext)));
+    } else {
+      return new Set([...extSet, ...G_EXTENSIONS]);
     }
   } else if (extSet.has(ext)) {
     const newExtSet = new Set(extSet);
@@ -88,12 +100,15 @@ const getISA = () => {
 const Instruction = ({info}) => {
   return (
     <div className="insn margin-top margin-bottom">
-      <div class="asm">
+      <div className="asm">
         <strong><code>{info['asm']}</code></strong>
       </div>
-      <div class="margin-top">
-        {info['desc'] ? <div className="insn-desc">{info['desc'].split("\n").map(line => (
-          <p>{line}</p>
+      {info['real'] ? <div className="margin-top">
+          <code>{info['real']}</code>
+        </div> : null}
+      <div className="margin-top">
+        {info['desc'] ? <div className="insn-desc">{info['desc'].split("\n").map((line, index) => (
+          <div key={index} dangerouslySetInnerHTML={{__html: marked.parse(line)}}></div>
         ))}</div> : null}
         {info['code'] ? <pre className="margin-top">
           <code>{info['code']}</code>
@@ -103,7 +118,7 @@ const Instruction = ({info}) => {
           <div><strong>Pseudoinstructions:</strong></div>
           <div className="margin-top margin-left-20">
             {info['pseudos'].map((info, index) => (
-                <div className="margin-top-20">
+                <div key={info.name} className="margin-top-20">
                   <div className="asm">
                     <strong><code>{info['asm']}</code></strong>
                   </div>
@@ -124,93 +139,104 @@ const Instruction = ({info}) => {
   )
 }
 
-const notation = `notations:
-
-    x, signed(x): default treated x as a signed number
-     unsigned(x): treated x as a unsigned number
-sign_extended(x): sign extend x to XLEN bits number
-    rd, rs1, rs2: [x0, x31]
-           imm12: [-2^11, 2^11-1] i.e. [-2048, 2047]
-           imm20: [-2^19, 2^19-1] i.e. [-524288, 524287]
-           uimm5: [0, 2^5-1] i.e. [0, 31]
-           uimm6: [0, 2^6-1 i.e. [0, 63]
+const notation = `\
+  rd, rs1, rs2: [x0, x31]
+         imm12: [-2^11, 2^11-1] i.e. [-2048, 2047]
+         imm20: [-2^19, 2^19-1] i.e. [-524288, 524287]
+         uimm5: [0, 2^5-1] i.e. [0, 31]
+         uimm6: [0, 2^6-1 i.e. [0, 63]
+  x, signed(x): default treated x as a signed number
+   unsigned(x): treated x as a unsigned number
+sign_extend(x): sign extend x to XLEN bits number
 `
 
 const App = () => {
   const [xlen, setXLEN] = React.useState("RV32");
   const [ISA, setISA] = React.useState(null);
   const [extVisible, setExtVisible] = React.useState({});
-  const [extSet, setExtSet] = React.useState(new Set(RISCV_EXTENSIONS));
+  const [extSet, setExtSet] = React.useState(new Set(G_EXTENSIONS));
   React.useEffect(() => {
     getISA().then(ISA => {
       setISA(ISA);
     })
   }, []);
-  return <>
-    <div className="container">
-      <h1 className="row row-no-gutters">RISC-V Instruction Sets</h1>
-      <div className="row row-no-gutters">
-        <div className="col-xs-4 col-md-2 radio">
-          <label>
-            <input type="radio" checked={xlen == "RV32"} onChange={() => setXLEN("RV32")} />
-            RV32
-          </label>
-        </div>
-        <div className="col-xs-4 col-md-2 radio">
-          <label>
-            <input type="radio" checked={xlen == "RV64"} onChange={() => setXLEN("RV64")} />
-            RV64
-          </label>
-        </div>
+  return <div className="container">
+    <h1 className="row row-no-gutters">RISC-V Instruction Sets</h1>
+    <div className="row row-no-gutters">
+      <div className="col-xs-4 col-md-2 radio">
+        <label>
+          <input type="radio" checked={xlen == "RV32"} onChange={() => setXLEN("RV32")} />
+          RV32
+        </label>
       </div>
-      <div className="flexbox">
-        <div className="checkbox margin-right-20">
-          <label>
-            <input type="checkbox" checked={isAll(extSet)} onChange={() => setExtSet(toggle(extSet, "all"))} />
-            ALL
-          </label>
-        </div>
-        {Array.from(RISCV_EXTENSIONS).map(ext => (
-          <div className="checkbox margin-right-20" key={ext}>
-            <label>
-              <input type="checkbox" checked={extSet.has(ext)} onChange={() => setExtSet(toggle(extSet, ext))} />
-              {ext}
-            </label>
-          </div>
-        ))}
+      <div className="col-xs-4 col-md-2 radio">
+        <label>
+          <input type="radio" checked={xlen == "RV64"} onChange={() => setXLEN("RV64")} />
+          RV64
+        </label>
       </div>
-      <pre><code>{notation}</code></pre>
-      {ISA
-        ? Array.from(RISCV_EXTENSIONS).filter(ext => extSet.has(ext)).map(ext => ISA[xlen][ext]).map(extInfo => (
-          <div className="panel panel-success margin-top-20">
-            <div
-              className="panel-heading" 
-              onClick={() => {
-                setExtVisible({
-                  ...extVisible, 
-                  [extInfo.name]: !(extVisible[extInfo.name] || extVisible[extInfo.name] === undefined)
-                })
-              }} >
-                <h3 class="panel-title">
-                  <strong>{extInfo.name}</strong>
-                  <small>&nbsp;&nbsp;&nbsp;&nbsp;{extInfo.meta?.version}</small>
-                </h3>
-                {extInfo.meta?.desc ? <div className="margin-top"><small>{extInfo.meta.desc}</small></div> : null}
-              </div>
-              {extVisible[extInfo.name] === false 
-                ? null 
-                : <ul class="list-group">
-                    {extInfo.insns.map((info) => (
-                      <li class="list-group-item">
-                        <Instruction info={info} />
-                      </li>
-                    ))}
-                  </ul>}
-          </div>
-      )) : null}
     </div>
-  </>
+    <div className="flexbox">
+      <div className="checkbox margin-right-20">
+        <label>
+          <input type="checkbox" checked={isAll(extSet)} onChange={() => setExtSet(toggle(extSet, "all"))} />
+          ALL
+        </label>
+      </div>
+      <div className="checkbox margin-right-20">
+        <label>
+          <input type="checkbox" checked={hasG(extSet)} onChange={() => setExtSet(toggle(extSet, "g"))} />
+          G
+        </label>
+      </div>
+      {Array.from(RISCV_EXTENSIONS).map(ext => (
+        <div className="checkbox margin-right-20" key={ext}>
+          <label>
+            <input type="checkbox" checked={extSet.has(ext)} onChange={() => setExtSet(toggle(extSet, ext))} />
+            {ext}
+          </label>
+        </div>
+      ))}
+    </div>
+    <p>-march={Array.from(extSet).sort((a, b) => a.length - b.length).map(ext => (ext.length > 1 ? `_` : '') + ext).join("")}</p>
+    <div className="panel panel-info margin-top">
+      <div className="panel-heading">Notations</div>
+      <div className="panel-body">
+        <pre><code>{notation}</code></pre>
+      </div>
+    </div>
+    <p><small>Note: The descriptions of the instructions are mostly from <a href="https://riscv.org/technical/specifications/">the RISC-V ISA specification</a>.</small></p>
+    {ISA
+      ? Array.from(RISCV_EXTENSIONS).filter(ext => extSet.has(ext)).map(ext => ISA[xlen][ext]).map(extInfo => (
+        <div key={extInfo.name} className="panel panel-success margin-top-20">
+          <div
+            className="panel-heading"
+            onClick={() => {
+              setExtVisible({
+                ...extVisible, 
+                [extInfo.name]: !(extVisible[extInfo.name] || extVisible[extInfo.name] === undefined)
+              })
+            }} >
+              <h3 className="panel-title">
+                <strong>{extInfo.name}</strong>
+                <small>&nbsp;&nbsp;&nbsp;&nbsp;{extInfo.meta?.version}</small>
+              </h3>
+              {extInfo.meta?.desc ? <div className="margin-top"><small>{extInfo.meta.desc}</small></div> : null}
+            </div>
+            {extVisible[extInfo.name] === false 
+              ? null 
+              : <ul className="list-group">
+                  {extInfo.insns.map((info) => (
+                    <li key={info.name} className="list-group-item">
+                      <Instruction info={info} />
+                    </li>
+                  ))}
+                </ul>}
+        </div>
+    )) : null}
+  </div>
 };
 
-const root = document.querySelector('#root');
-ReactDOM.render(<App />, root);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
