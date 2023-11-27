ec1a1ae85fcce14e84aabac8f8ada47dccab0e1a
Lehua Ding <lehuading@qq.com>
2023-11-27 14:38:56 +0800

    Update init-dev-env.sh

--- tools/init-dev-env.sh
+++ tools/init-dev-env.sh
@@ -11,5 +11,9 @@ git clone https://gitlab.com/lhtin-rivai/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh
 git clone https://gitlab.com/lhtin-rivai/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i -E "s/plugins=\(git\)/plugins=\(git zsh-autosuggestions zsh-syntax-highlighting\)/" ~/.zshrc
 
+# disable some git check which slow zsh
+git config --global --add oh-my-zsh.hide-status 1
+git config --global --add oh-my-zsh.hide-dirty 1
+
 # brew
 /bin/bash -c "$(curl -fsSL https://gitlab.com/lhtin-rivai/brew-install/-/raw/tin/install.sh)"
