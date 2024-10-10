911f0c9be04f25c0ded13b8bb3e6482f1b950b7c
Lehua Ding <lehuading@qq.com>
2024-10-10 11:43:33 +0800

    Update init-env.sh

--- tools/init-env.sh
+++ tools/init-env.sh
@@ -6,7 +6,8 @@ set -x
 set -o pipefail
 
 # ohmyzsh, zsh-autosuggestions, zsh-syntax-highlighting
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv .oh-my-zsh .oh-my-zsh-bak
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i -E "s/plugins=\(git\)/plugins=\(git zsh-autosuggestions zsh-syntax-highlighting\)/" ~/.zshrc
